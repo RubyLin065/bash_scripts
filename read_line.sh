846666b5dc47e007892e318847a77d24d046a70f
RubyLin065 <86210496+RubyLin065@users.noreply.github.com>
2021-07-13 12:47:53 +0800

    1.1

---
+++ read_line.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# this file will output series of command to
+# use python to control the news web scraping 
+# wish list will perform like following
+# `cat Data/wishlist.txt` >> 台郡 聯發科 元大 國泰金 鴻海
+#
+declare -c python_script;
+python_script = "scrp_news_copy.py";
+./argsum.sh `cat Data/wishlist.txt` > wishcount_tmp.tmp
+
+echo "sleep 1; " > cmdline.sh
+
+while read -r line;
+do
+		if (( `echo ${#line}` >= 1 ))
+		then
+			echo "python scrp_news_copy.py $line &" >> cmdline.sh
+		fi
+done < Data/wishlist.txt
+echo "sleep 60" >> cmdline.sh
+#exe
+./cmdline.sh
+sleep 10;
+rm cmdline.sh
+
+# for i in $@
+# 	echo "python scrp_new_copy.py $i "
+# done
+
